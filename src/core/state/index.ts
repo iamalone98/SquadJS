504343ec535ec12329fdf428ddaa528608cf6889
I-Am Alone <iamalone9828@gmail.com>
2023-12-16 08:46:04 +0300

    add: state updaters interval

--- src/core/state/index.ts
+++ src/core/state/index.ts
@@ -1,3 +1,4 @@
+import { serversState } from '../../serversState';
 import { updateCurrentMap } from './updateCurrentMap';
 import { updateNextMap } from './updateNextMap';
 import { updatePlayers } from './updatePlayers';
@@ -8,4 +9,19 @@ export const initState = async (id: number) => {
   await updateSquads(id);
   await updateCurrentMap(id);
   await updateNextMap(id);
+
+  const { listener } = serversState[id];
+
+  setInterval(() => {
+    updatePlayers(id);
+  }, 30000);
+
+  setInterval(() => {
+    updateSquads(id);
+  }, 30000);
+
+  listener.on('NEW_GAME', () => {
+    updateCurrentMap(id);
+    updateNextMap(id);
+  });
 };
