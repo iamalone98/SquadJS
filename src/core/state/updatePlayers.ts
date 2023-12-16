876432206e9b293feccaa13dc8cfd880d6933a85
I-Am Alone <iamalone9828@gmail.com>
2023-12-16 08:42:49 +0300

    update: updatePlayers

--- src/core/state/updatePlayers.ts
+++ src/core/state/updatePlayers.ts
@@ -10,7 +10,20 @@ export const updatePlayers = async (id: number) => {
 
   return new Promise((res) => {
     listener.once('ListPlayers', (data: TPlayer[]) => {
-      serversState[id].players = [...data];
+      serversState[id].players = data.map((player) => {
+        const playerFound = serversState[id].players?.find(
+          (p) => p.steamID === player.steamID,
+        );
+
+        if (playerFound) {
+          return {
+            ...playerFound,
+            ...player,
+          };
+        }
+
+        return player;
+      });
 
       res(true);
     });
